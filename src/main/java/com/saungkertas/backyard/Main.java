b56881e2cf0f76fa4e3c83b3dbef78dc48143084
syarifhidayatullah <syarif.h@go-jek.com>
2018-09-08 16:06:24 +0700

    Updated main class, define grid with Hashmap<Object, Integer>

--- src/main/java/com/saungkertas/backyard/Main.java
+++ src/main/java/com/saungkertas/backyard/Main.java
@@ -7,16 +7,17 @@ public class Main {
     public static void main(String[] args) {
         int matrix = 10;
 
-        HashMap grid = new HashMap();
+
+        HashMap<Object, Integer> grid = new HashMap<>();
         for (int x = 0; x < matrix; x++) {
             for (int y = 0; y < matrix; y++) {
-                grid.put(new Point(x, y), 0);
+                grid.put(new Point(x, y), 1);
             }
         }
 
         for (int x = 0; x < matrix; x++) {
             for (int y = 0; y < matrix; y++) {
-                if (grid.get(new Point(x, y)).equals(0)) {
+                if (grid.get(new Point(x, y)) == 0) {
                     System.out.print("   ");
                 } else {
                     System.out.print(" * ");
