e0369031664c0ba5770600218fec7c9735e7910e
syarifhidayatullah <syarif.h@go-jek.com>
2018-09-08 17:50:55 +0700

    Updated main class, integrate with liveordie

--- src/main/java/com/saungkertas/backyard/Main.java
+++ src/main/java/com/saungkertas/backyard/Main.java
@@ -5,19 +5,24 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        int matrix = 10;
-
+        int matrix = 20;
 
         HashMap<Object, Integer> grid = new HashMap<>();
-        for (int x = 0; x < matrix; x++) {
-            for (int y = 0; y < matrix; y++) {
-                grid.put(new Point(x, y), 1);
+        HashMap<Object, Integer> nextGrid = new HashMap<>();
+        for (int x = -1; x <= matrix; x++) {
+            for (int y = -1; y <= matrix; y++) {
+                grid.put(new Point(x, y), 0);
+                nextGrid.put(new Point(x, y), 0);
             }
         }
 
+        grid.put(new Point(3, 2), 1);
+        grid.put(new Point(3, 3), 1);
+        grid.put(new Point(3, 4), 1);
+
         for (int x = 0; x < matrix; x++) {
             for (int y = 0; y < matrix; y++) {
-                if (grid.get(new Point(x, y)) == 0) {
+                if (getPointValue(grid, x, y) == 0) {
                     System.out.print("   ");
                 } else {
                     System.out.print(" * ");
@@ -25,5 +30,46 @@ public class Main {
             }
             System.out.println();
         }
+
+        while (true) {
+            for (int x = 0; x < matrix; x++) {
+                for (int y = 0; y < matrix; y++) {
+                    int aliveNeighbours = getPointValue(grid, x - 1, y - 1) +
+                            getPointValue(grid, x - 1, y) +
+                            getPointValue(grid, x - 1, y + 1) +
+                            getPointValue(grid, x, y - 1) +
+                            getPointValue(grid, x, y + 1) +
+                            getPointValue(grid, x + 1, y - 1) +
+                            getPointValue(grid, x + 1, y) +
+                            getPointValue(grid, x + 1, y + 1);
+
+                    LiveOrDie liveOrDie = new LiveOrDie(getPointValue(grid, x, y), aliveNeighbours);
+                    int status = liveOrDie.isAlive();
+                    nextGrid.put(new Point(x, y), status);
+                }
+            }
+
+            grid = nextGrid;
+
+            for (int x = 0; x < matrix; x++) {
+                for (int y = 0; y < matrix; y++) {
+                    if (getPointValue(grid, x, y) == 0) {
+                        System.out.print("   ");
+                    } else {
+                        System.out.print(" * ");
+                    }
+                }
+                System.out.println();
+            }
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Integer getPointValue(HashMap<Object, Integer> grid, int x, int y) {
+        return grid.get(new Point(x, y));
     }
 }
